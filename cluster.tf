e6eb0938cada394bf0612ea8701bb31582d040da
Ashik Zebulun <ashikzebulun.cn@gmail.com>
2025-01-12 20:54:03 +0000

    updated

--- cluster.tf
+++ cluster.tf
@@ -1,6 +1,6 @@
 module "aro_cluster" {
   source                   = "app.terraform.io/az/module/aro"
-  version                  = "1.0.6"
+  version                  = "1.0.7"
   cluster_location         = "uks"
   cluster_environment      = "dev"
   cluster_count            = 1
